26bf73673be2fcc3fbc6745e75f6ef97ada670f5
DKLokeswaran <lokeswarankandasamy12@gmail.com>
2022-06-29 19:05:32 +0530

    reset

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -16,9 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button numberList[],symbolList[],clearButton;
     private TextView questionBox;
-    private ArrayList<Integer> integerArray=new ArrayList<>();
-    private ArrayList<Double> doubleArray=new ArrayList<>();
-    private ArrayList<String> operators=new ArrayList<>();
+
+
     private String question;
     private int pos;
     private ImageButton backSpace;
@@ -34,60 +33,6 @@ public class MainActivity extends AppCompatActivity {
         clearButton=findViewById(R.id.button);
         questionBox=findViewById(R.id.textView);
         backSpace=findViewById(R.id.button17);
-        question="";
-        pos=1;
-        integerArray.add(new Integer(pos));
-        //doubleArray.add(new Double());
-        for(Button button:numberList ){
-            button.setOnClickListener(v -> {
-                int key= java.lang.Integer.parseInt(button.getText().toString());
-                boolean temp;
-                if((!integerArray.isEmpty())&&integerArray.get(integerArray.size()-1).getPos()==pos){
-                    temp=integerArray.get(integerArray.size()-1).updateNumber(key);
-                }
-                else{
-                    temp=doubleArray.get(doubleArray.size()-1).updateDecimal(key);
-                }
-                //boolean temp=integerArray.get(integerArray.size()-1).updateNumber(key);
-                if(!temp){
-                    Toast.makeText(this, "large", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    question+=button.getText();
-                    questionBox.setText(question);
-                }
-
-            });
-        }
-        for(Button button:symbolList){
-            button.setOnClickListener(v -> {
-                question+=button.getText();
-                questionBox.setText(question);
-                if(button.getText().toString().equals(".") &&integerArray.get(integerArray.size()-1).getPos()==pos){
-                    doubleArray.add(new Double(integerArray.get(integerArray.size()-1)));
-                    integerArray.remove(integerArray.size()-1);
-                }
-                else if(isOperator(button.getText().toString())){
-                    pos++;
-                    integerArray.add(new Integer(pos));
-                }
-                if(isOperator(button.getText().toString())||isBracket(button.getText().toString())){
-                    operators.add(button.getText().toString());
-                }
-            });
-        }
-        clearButton.setOnClickListener(v -> {
-            question="";
-            questionBox.setText(question);
-            integerArray.clear();
-            doubleArray.clear();
-            operators.clear();
-            pos=1;
-            integerArray.add(new Integer(pos));
-            doubleArray.add(new Double());
-
-        });
-
 
 
     }
